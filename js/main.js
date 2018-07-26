3c219dea91cc2ebfbc73f5bd3f27245c090605bf
Patrick Mexal <patrickmexal@gmail.com>
2018-07-26 11:59:34 -0700

    initial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+	$('#mySidenav').css('width', '250px');
+    $('#main').css('margin-left', '250px');
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    $('#mySidenav').css('width', '0');
+    $('#main').css('margin-left', '0');
+}
